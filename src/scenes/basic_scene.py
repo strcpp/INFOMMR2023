106b4c376ce98302c4b129235dc4e25a856dd552
abed <abed.farhat96@gmail.com>
2023-09-14 14:46:41 +0200

    skybox and imgui

--- src/scenes/basic_scene.py
+++ src/scenes/basic_scene.py
@@ -20,6 +20,7 @@ class BasicScene(Scene):
     grid = None
 
     def load(self) -> None:
+        self.skybox = Skybox(self.app, skybox='clouds', ext='png')
         pass
 
     def unload(self) -> None:
@@ -32,8 +33,24 @@ class BasicScene(Scene):
         """
         Renders the UI.
         """
+        imgui.new_frame()
+
+        # Change the style of the entire ImGui interface
+        imgui.style_colors_classic()
+
+        # Add an ImGui window
+        imgui.begin("Settings", flags=imgui.WINDOW_ALWAYS_AUTO_RESIZE)
+
+        imgui.text("Click and drag left/right mouse button to rotate camera.")
+        imgui.text("Click and drag middle mouse button to pan camera.")
+        imgui.end()
+        imgui.render()
+
+        self.app.imgui.render(imgui.get_draw_data())
 
     def render(self) -> None:
+        self.skybox.draw(self.app.camera.projection.matrix, self.app.camera.matrix)
+        self.render_ui()
         """
         Renders all objects in the scene.
         """
