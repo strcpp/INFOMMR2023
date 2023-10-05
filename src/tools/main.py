1e68223cfd21351b314c4b2160c0561a92f782eb
Charalampos Georgiadis <georgfx@gmail.com>
2023-10-05 14:46:22 +0200

    Minor change

--- src/tools/main.py
+++ src/tools/main.py
@@ -3,8 +3,6 @@ from display_statistics import save_histograms
 
 
 def main():
-    csv_file_path = os.path.join('src', 'tools', 'outputs', 'shape_data.csv')
-
     save_histograms(show_histogram=False)
 
 
